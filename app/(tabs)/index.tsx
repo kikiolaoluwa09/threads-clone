3cb14c20d38bcefcf1c2ed7d16f176b3cc432ca9
Kikiolaoluwa Adeniran <adeniran.kikiolaoluwa0409@gmail.com>
2025-12-28 14:32:44 +0100

    good progress so far

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import {Text, View } from "react-native";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World!</Text>
-        <Text style={styles.subtitle}>Feed</Text>
-      </View>
+    <View className="justify-center items-center flex-1">
+      <Text className="text-3xl font-bold text-red-500">Feed</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
