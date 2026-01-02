b76ce1ba7f098a2fcb465fb1df68eba7eccef155
Kikiolaoluwa Adeniran <adeniran.kikiolaoluwa0409@gmail.com>
2026-01-02 02:04:00 +0100

    minor changes

--- src/services/posts.ts
+++ src/services/posts.ts
@@ -17,6 +17,7 @@ export const fetchPosts = async () => {
   const { data, error } = await supabase
     .from("posts")
     .select("*, user:profiles(*), replies:posts(count)")
+    .order('created_at', {ascending: false})
     .throwOnError();
 
   console.log(JSON.stringify(data, null, 2));
